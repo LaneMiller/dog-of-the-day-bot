a809067b81b35ef0c1c9ebe3f36ef1ed6bd5aad4
github username <github email address>
2018-09-13 21:55:37 -0400

    finished work on multiple channel posting

--- dotd.js
+++ dotd.js
@@ -1,11 +1,12 @@
-
 document.addEventListener("DOMContentLoaded", (e) => {
   PIC_URL = `https://random.dog/woof.json`;
-  POST_URLS = config.POST_URLS;
+  KEY = config.API_KEY
+  POST_URLS = config.CHANNELS.map( channel =>
+    `https://slack.com/api/chat.postMessage?token=${KEY}&channel=%23${channel}&as_user=false&icon_emoji=%3Adog%3A&unfurl_media=true&username=Good%20Boi%20bot&pretty=1&text=`
+  )
+
   const posted = [];
   const datesPosted = [];
-
-  const startDate = new Date();
 
   fetchDogPic = (now) => {
     fetch(PIC_URL).then(res => res.json()).then(json => {
@@ -16,19 +17,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
       if (rightFileType && uniqueDoggo) {
         posted.push(json.url);
         datesPosted.push(`${now.getMonth()} ${now.getDate()}`);
-        // change config.POST_URLS to return an array of urls,
-        // then update postDogPic to take postUrl and picUrl
         POST_URLS.forEach( (postUrl) => {postDogPic(postUrl, json.url)} )
-        // postDogPic(json.url);
         console.log(`posted at ${now.getHours()}:${now.getMinutes()}`);
       } else {
         fetchDogPic();
       }
-    }).catch(error => {console.log(error); setTimeout(startApp, 60000)});
+    }).catch(error => {console.log(error); setTimeout(startApp, 10000)});
   }
 
   postDogPic = (postUrl, picUrl) => {
-    fetch(postUrl + `${picUrl} Here's your Dog of the Day!!`);
+    fetch(postUrl + `Here's your Dog of the Day!!   ${picUrl}`);
   }
 
   startApp = () => {
@@ -40,7 +38,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     if (msTillDog <= 0 && oneDogPerDay) {
       fetchDogPic(now);
     }
-    setTimeout(startApp, 3600000); //check again in 1 hour
+    setTimeout(startApp, 1800000); //check again in 1/2 hour
   }
 
   startApp()
